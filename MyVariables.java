9c6b195f1f4fc76deea33bd24c606b5d08060d97
shindepallu28 <pallavishinde127@gmail.com>
2019-01-13 16:01:42 +0530

    'update'

---
+++ MyVariables.java
@@ -0,0 +1,17 @@
+public class MyVariables{
+static int x = 50;
+static int y;
+public static void main(String args[]){
+System.out.println(x);
+System.out.println(y);
+y=100;
+System.out.println(y);
+System.out.println(y);
+x=80;
+System.out.println(x);
+}
+}
+
+
+
+
